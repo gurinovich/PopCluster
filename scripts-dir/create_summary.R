48f07a2ad87054625f6258cffbc39fc08f8a2ff0
Anastasia Gurinovich <gurinovichn@yahoo.com>
2017-11-30 13:43:13 -0500

    Update create_summary.R

--- scripts-dir/create_summary.R
+++ scripts-dir/create_summary.R
@@ -19,7 +19,6 @@ file.names <- dir(analysis.dir)
 
 for (i in 1:length(alleles)) {
   output <- data.frame(matrix(NA,nrow=length(file.names),ncol=16))
-#  names(output) <- c("Clusters","PC1.b","PC1.p","PC1.SE","PC2.b","PC2.p","PC2.SE","PC3.b","PC3.p","PC3.SE","PC4.b","PC4.p","PC4.SE","GLM.b","GLM.p","GLM.SE")
   names(output) <- c("Clusters","GLM.b","GLM.p","GLM.SE")
   output$Clusters <- gsub(".txt","",file.names)
   
@@ -29,14 +28,7 @@ for (i in 1:length(alleles)) {
     pca.names <- c("ID","PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10","PC11","PC12","PC13","PC14","PC15","PC16","PC17","PC18","PC19","PC20","PC21","PC22","PC23","PC24","PC25","PC26","PC27","PC28","PC29","PC30") 
     colnames(cluster) <- pca.names
     mega.data.cluster <- merge(cluster,mega.data,by="ID")  
-    # 
-    # for (k in 1:PCsNumber) {
-    #   mod <- lm(paste0("PC",k,"~",cov.string,alleles[i]), data=mega.data.cluster)
-    #   output[j,c(paste0("PC",k,".b"))] <-  as.numeric(coef(summary(mod))[,1][nrow(coef(summary(mod)))])  #betta
-    #   output[j,c(paste0("PC",k,".p"))] <-  as.numeric(coef(summary(mod))[,4][nrow(coef(summary(mod)))])  #p-value
-    #   output[j,c(paste0("PC",k,".SE"))] <- as.numeric(coef(summary(mod))[,2][nrow(coef(summary(mod)))]) # SE for the betta
-    # }
-    # 
+ 
     mod <- glm(paste0("phenotype~",cov.string,"PC1+PC2+PC3+PC4+",alleles[i]), family=binomial, data=mega.data.cluster)
     output[j,c("GLM.b")] <-  as.numeric(coef(summary(mod))[,1][nrow(coef(summary(mod)))])  #betta
     output[j,c("GLM.p")] <-  as.numeric(coef(summary(mod))[,4][nrow(coef(summary(mod)))])  #p-value
